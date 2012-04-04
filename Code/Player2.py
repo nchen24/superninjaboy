551ac6eb0b03184f63c862301c196ddb3d12d7dd
Tom Geheran <thomas.geheran@tufts.edu>
2012-04-04 00:25:38 -0400

    sliding on right side

--- Code/Player2.py
+++ Code/Player2.py
@@ -1,71 +1,37 @@
-import pygame, os, sys
+import pygame, os, sys, Spritesheet
 from pygame.locals import *
+from Spritesheet import *
 
 ###############################
 # Constants for image loading #
 ###############################
-IDLE1 = "../assets/idle.gif"
-FALL1 = "../assets/fall.gif"
-JUMP1 = "../assets/jump.gif"
-WALK1 = "../assets/walking_1.gif"
-WALK2 = "../assets/walking_2.gif"
-WALK3 = "../assets/walking_3.gif"
-WALK4 = "../assets/walking_4.gif"
-RUNN1 = "../assets/running_1.gif"
-RUNN2 = "../assets/running_2.gif"
-RUNN3 = "../assets/running_3.gif"
-RUNN4 = "../assets/running_4.gif"
-SLID1 = "../assets/wall_slide.gif"
-PLAT1 = "../assets/platform1.png"
-#SS = "../assets/short_sprite_sheet_white_back.gif"
-SS = "../assets/short_sprite_sheet_white_back.png"
+SS = "../assets/player_standard.png"
+
+IDLE_L = (0,25,18,23)
+FALL_L = (180,25,18,23) 
+JUMP_L = (160,25,18,23)
+WALK_L = ((20,25,18,23),(40,25,18,23),(60,25,18,23),(80,25,18,23))
+RUNN_L = ((100,25,18,23),(120,25,18,23),(140,25,18,23))
+SLID_L = (200,25,18,23)
+
+IDLE_R = (0,0,18,23)
+FALL_R = (180,0,18,23)
+JUMP_R = (160,0,18,23)
+WALK_R = ((20,0,18,23),(40,0,18,23),(60,0,18,23),(80,0,18,23)) 
+RUNN_R = ((100,0,18,23),(120,0,18,23),(140,0,18,23))
+SLID_R = (200,0,18,23)
 
 ###################
 # Other constants #
 ###################
-ONEFRAME  = 20
-JUMPLIMIT = 100
+ONEFRAME = 11
+JUMPLIMIT = 50
 SPRINTMOD = 2
-
-
-
-class spritesheet(object):
-    def __init__(self, filename):
-        try:
-            self.sheet = pygame.image.load(filename).convert()
-        except pygame.error, message:
-            print 'Unable to load spritesheet image:', filename
-            raise SystemExit, message
-    # Load a specific image from a specific rectangle
-    def image_at(self, rectangle, colorkey = None):
-        "Loads image from x,y,x+offset,y+offset"
-        rect = pygame.Rect(rectangle)
-        image = pygame.Surface(rect.size).convert()
-        image.blit(self.sheet, (0, 0), rect)
-        if colorkey is not None:
-            if colorkey is -1:
-                colorkey = image.get_at((0,0))
-            image.set_colorkey(colorkey, pygame.RLEACCEL)
-        return image
-    # Load a whole bunch of images and return them as a list
-    def images_at(self, rects, colorkey = None):
-        "Loads multiple images, supply a list of coordinates" 
-        return [self.image_at(rect, colorkey) for rect in rects]
-    # Load a whole strip of images
-    def load_strip(self, rect, image_count, colorkey = None):
-        "Loads a strip of images and returns them as a list"
-        tups = [(rect[0]+rect[2]*x, rect[1], rect[2], rect[3])
-                for x in range(image_count)]
-        return self.images_at(tups, colorkey)
-
-
-
-
+RES = (1024, 768)
 
 #################
 # Player Sprite #
 #################
-
 
 class Player(pygame.sprite.Sprite):
     def load_image(self, image_name):
@@ -79,30 +45,45 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, screen, x, y):
 
         self.ss = spritesheet(SS)
-        self.WALK = []
-        self.RUNN = []
+        self.WALK_RIGHT = []
+        self.WALK_LEFT = []
+        self.RUNN_RIGHT = []
+        self.RUNN_LEFT = []
+
         #Load two images into an array, their transparent bit is (255, 255, 255)
-        self.WALK = self.ss.images_at(((29, 9, 15, 26),(51, 9, 10,26),(68,9,12,26),(88,10,14,25)), colorkey=-1)
-        self.IDLE = self.ss.image_at((9,9,12,26),colorkey=-1)
-        self.RUNN = self.ss.images_at(((110,13,18,22),(136,12,13,23),(157,11,13,24),(178,15,20,20)), colorkey=-1)
-        self.JUMP = self.ss.image_at((206,15,17,20),colorkey=-1)
-        self.FALL = self.ss.image_at((231,13,20,22),colorkey=-1)
-        self.SLID = self.ss.image_at((257,11,13,24),colorkey=-1)
+        self.WALK_RIGHT = self.ss.images_at(WALK_R, colorkey = -1)
+        self.WALK_LEFT  = self.ss.images_at(WALK_L, colorkey = -1)
+
+        self.RUNN_RIGHT = self.ss.images_at(RUNN_R, colorkey = -1)
+        self.RUNN_LEFT  = self.ss.images_at(RUNN_L, colorkey = -1)
+
+        self.JUMP_RIGHT = self.ss.image_at(JUMP_R,  colorkey = -1)
+        self.JUMP_LEFT  = self.ss.image_at(JUMP_L,  colorkey = -1)
+
+        self.IDLE_RIGHT = self.ss.image_at(IDLE_R,  colorkey = -1)
+        self.IDLE_LEFT  = self.ss.image_at(IDLE_L,  colorkey = -1)
         
+        self.FALL_RIGHT = self.ss.image_at(FALL_R,  colorkey = -1)
+        self.FALL_LEFT  = self.ss.image_at(FALL_L,  colorkey = -1)
+
+        self.SLID_RIGHT = self.ss.image_at(SLID_R,  colorkey = -1)
+        self.SLID_LEFT  = self.ss.image_at(SLID_L,  colorkey = -1)
 
         self.direction = "Right"
-        #self.image = self.load_image(IDLE1)
-        self.loadImage(self.IDLE)
+        self.whichIdle()
 
         self.screen = screen
         self.x   = x
         self.y   = y
         self.dx  = 0
         self.dy  = 0
-        self.ddx = .1
+
+        self.ddx = .5
         self.ddy = 0
-        self.dx_max = .8
-        self.dy_max = .8
+
+        self.dx_max = 2
+        self.dy_max = 2
+
         self.image_w, self.image_h = self.image.get_size()
 
         self.rect = self.image.get_rect()
@@ -110,50 +91,63 @@ class Player(pygame.sprite.Sprite):
         self.rect.topleft = (self.x, self.y)
         self.rect.bottomright = (self.x + self.image_w, self.y + self.image_h)
 
-        self.bottom = pygame.Rect((self.x,(self.y + self.image_h - 5)), (self.image_w, 5))
+        self.bottom = pygame.Rect((self.x + 7,(self.y + self.image_h - 14)), (self.image_w - 4, 5))
 
         # pygame.Rect takes ((start_x, start_y), (width, height))[EXPERIMENTAL]
-        self.boundwidth = 5
-        self.left = pygame.Rect((self.x, self.y), (self.boundwidth, self.image_h))
-        self.right = pygame.Rect((self.x + self.image_w - self.boundwidth, self.y), (self.boundwidth, self.image_h))
+        self.boundwidth = 2
+        self.left = pygame.Rect((self.x, self.y), (self.boundwidth, self.image_h-self.boundwidth))
+        self.right = pygame.Rect((self.x + self.image_w - self.boundwidth, self.y), (self.boundwidth, self.image_h - self.boundwidth))
 
         self.alive = True
         self.jumped = False
+        self.canJump = True
         self.apex = False # True if the player has reached the max jump height
         self.jumptimer = 0
-        #self.direction = "Right"
         self.movetimer = 0
         self.runtimer  = 0
         self.onplat = False
         self.onwall = False
-
-        
-
+        self.contact_side = None
+        self.wallJump_Left = False
+        self.wallJump_Right = False
 
     def draw(self):
         self.screen.blit(self.image, (self.x, self.y))
 
     def update(self, pressed, screenDimensions):
-        if pressed['Space'] == True and not(self.jumped) and not(self.apex):
+
+        if self.x < 0 or self.x > RES[0] or self.y < 0 or self.y > RES[1]:
+            self.alive = False
+
+        if pressed['Space'] == True and not(self.jumped) and not(self.apex) and self.canJump == True:
             if self.jumptimer > JUMPLIMIT:
                 self.apex = True
+                self.canJump = False
+                self.wallJump_Left = False
+                self.wallJump_Right = False
             else:
                 self.jumptimer += 1
 
-            #self.loadImage(JUMP1)
-            self.loadImage(self.JUMP)
-            self.dy = -.8
+            self.whichJump()
+            self.dy = -2
 
-        elif self.rect.bottomright[1] < screenDimensions[1] and self.onplat == False and self.onwall == False:
-            #self.loadImage(FALL1)
-            self.loadImage(self.FALL)
-            self.dy = .8
+            if self.onplat == True:
+                print("on dat plat")
 
-        
+            if self.wallJump_Left == True and self.onplat == False:
+                self.x += -2
+            elif self.wallJump_Right == True and self.onplat == False:
+                self.x += 2
+                print("Go Right!")
+
+            self.onplat = False
+
+        elif self.onplat == False and self.onwall == False:
+            self.whichFall()
+            self.dy = 2
+
         else:
-            #self.loadImage(IDLE1)
-            self.loadImage(self.IDLE)
-
+            self.whichIdle()
             self.dy = 0
             self.jumped = False
             self.apex = False
@@ -199,20 +193,55 @@ class Player(pygame.sprite.Sprite):
         else:
             self.dx = 0
 
-        if self.onwall == True:
-            #if self.direction == "Right":
-            #    self.loadImage(SLID1, "Left")
-            #else: self.loadImage(SLID1, "Right")
-            #self.loadImage(SLID1)
-            self.loadImage(self.SLID)
-            self.dy = .2
-            self.dx = 0
-            self.jumped = False
-            self.apex = False
-            self.jumptimer = 0
+        #HANDLES JUMPING OFF OF A WALL
+        if self.onwall == True and self.onplat == False:
+
+            if self.contact_side == "Left" and pressed['Right']:
+                self.dx = 2
+
+            elif self.contact_side == "Right" and pressed['Left']:
+                self.dx = -2
+
+            elif self.contact_side == "Left" and pressed['Space'] and not(self.jumped) and not(self.apex) and self.canJump == True:
+                self.direction = "Right"
+                self.onplat = False
+                if self.jumptimer > JUMPLIMIT:
+                    self.apex = True
+                    self.canJump = False
+                else:
+                    self.jumptimer += 1
+
+                self.image = self.JUMP_RIGHT
+                self.dy = -2
+                self.dx = 2
+                self.wallJump_Right = True
+
+            elif self.contact_side == "Right" and pressed['Space'] and not(self.jumped) and not(self.apex) and self.canJump == True:
+                self.direction = "Left"
+                self.onplat = False
+                if self.jumptimer > JUMPLIMIT:
+                    self.apex = True
+                    self.canJump = False
+                else:
+                    self.jumptimer += 1
+
+                self.image = self.JUMP_LEFT
+                self.dy = -2
+                self.dx = -2
+                self.wallJump_LEFT = True
+
+            else:
+                self.whichSlide()
+                self.dy = 1.5
+                self.dx = 0
+                self.jumped = False
+                self.apex = False
+                self.jumptimer = 0
+                self.canJump = True
 
 
         # Move the bounding box
+        self.image_w, self.image_h = self.image.get_size()
         self.x = self.x + self.dx
         self.y = self.y + self.dy
         self.rect = self.image.get_rect()
@@ -227,25 +256,47 @@ class Player(pygame.sprite.Sprite):
         self.right.top = self.right.top + self.dy
         self.right.left = self.right.left + self.dx
 
-        self.bottom = pygame.Rect((self.x,(self.y + self.image_h - 5)), (self.image_w, 5))
+        self.left = pygame.Rect((self.x, self.y), (self.boundwidth, self.image_h - self.boundwidth))
+        self.right = pygame.Rect((self.x + self.image_w - self.boundwidth, self.y), (self.boundwidth, self.image_h - self.boundwidth))
+        self.bottom = pygame.Rect((self.x+5,(self.y + self.image_h - 5)), (self.image_w-10, 5))
+
+    def respawn(self, x, y):
+        self.x = x
+        self.y = y
+        self.jumped = False
+        self.jumptimer = 0
+        self.onplat = False
+        self.whichIdle()
+        self.dx  = 0
+        self.dy  = 0
+        self.alive = True
 
     # Determines which walking animation to load.
     def whichWalk(self):
         if self.movetimer < ONEFRAME: # ONEFRAME is defined in constants.  This
-            #self.loadImage(WALK1)     # allows for easy tweaking of animation
-            self.loadImage(self.WALK[0])
+            # allows for easy tweaking of animation
+            if self.direction == "Right":
+                self.image = self.WALK_RIGHT[0]
+            elif self.direction == "Left":
+                self.image = self.WALK_LEFT[0]
             self.movetimer += 1
         elif self.movetimer < 2*ONEFRAME: 
-            #self.loadImage(WALK2)
-            self.loadImage(self.WALK[1])
+            if self.direction == "Right":
+                self.image = self.WALK_RIGHT[1]
+            elif self.direction == "Left":
+                self.image = self.WALK_LEFT[1]
             self.movetimer += 1
         elif self.movetimer < 3*ONEFRAME:
-            #self.loadImage(WALK3)
-            self.loadImage(self.WALK[2])
+            if self.direction == "Right":
+                self.image = self.WALK_RIGHT[2]
+            elif self.direction == "Left":
+                self.image = self.WALK_LEFT[2]
             self.movetimer += 1
         elif self.movetimer < 4*ONEFRAME:
-            #self.loadImage(WALK4)
-            self.loadImage(self.WALK[3])
+            if self.direction == "Right":
+                self.image = self.WALK_RIGHT[3]
+            elif self.direction == "Left":
+                self.image = self.WALK_LEFT[3]
             self.movetimer += 1
         else:
             self.movetimer = 0
@@ -253,31 +304,57 @@ class Player(pygame.sprite.Sprite):
     # Determines which running animation to load.
     def whichRun(self):
         if self.runtimer < ONEFRAME:
-            #self.loadImage(RUNN1)
-            self.loadImage(self.RUNN[0])
+            if self.direction == "Right":
+                self.image = self.RUNN_RIGHT[0]
+            elif self.direction == "Left":
+                self.image = self.RUNN_LEFT[0]
             self.runtimer += 1
         elif self.runtimer < 2*ONEFRAME: 
-            #self.loadImage(RUNN2)
-            self.loadImage(self.RUNN[1])
+            if self.direction == "Right":
+                self.image = self.RUNN_RIGHT[1]
+            elif self.direction == "Left":
+                self.image = self.RUNN_LEFT[1]
             self.runtimer += 1
         elif self.runtimer < 3*ONEFRAME:
-            #self.loadImage(RUNN3)
-            self.loadImage(self.RUNN[2])
-            self.runtimer += 1
-        elif self.runtimer < 4*ONEFRAME:
-            #self.loadImage(RUNN4)
-            self.loadImage(self.RUNN[3])
+            if self.direction == "Right":
+                self.image = self.RUNN_RIGHT[2]
+            elif self.direction == "Left":
+                self.image = self.RUNN_LEFT[2]
             self.runtimer += 1
         else:
             self.runtimer = 0
 
-    # Takes in state and loads the appropriate image.
-    def loadImage(self, state):
+    def whichIdle(self):
         if self.direction == "Right":
-            #self.image = self.load_image(state)
-            self.image = state
+            self.image = self.IDLE_RIGHT
         elif self.direction == "Left":
-            #self.image = pygame.transform.flip(self.load_image(state), True, False)
-            self.image = pygame.transform.flip(state,True,False)
+            self.image = self.IDLE_LEFT
+
+    def whichJump(self):
+        if self.direction == "Right":
+            self.image = self.JUMP_RIGHT
+        elif self.direction == "Left":
+            self.image = self.JUMP_LEFT
+
+    def whichFall(self):
+        if self.direction == "Right":
+            self.image = self.FALL_RIGHT
+        elif self.direction == "Left":
+            self.image = self.FALL_LEFT
+
+    def whichSlide(self):
+        if self.contact_side == "Right":
+            self.image = self.SLID_RIGHT
+        elif self.contact_side == "Left":
+            self.image = self.SLID_LEFT
+
+    ## Takes in state and loads the appropriate image.
+    #def loadImage(self, state):
+    #    if self.direction == "Right":
+    #        #self.image = self.load_image(state)
+    #        self.image = state
+    #    elif self.direction == "Left":
+    #        #self.image = pygame.transform.flip(self.load_image(state), True, False)
+    #        self.image = pygame.transform.flip(state,True,False)
 
         
